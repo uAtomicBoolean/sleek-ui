8e29c89c2f36a06bf36f9f0a395efbb0f72fc30a
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2025-11-21 13:44:51 +0100

    Comment obsolete code

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let ui = AppWindow::new()?;
 
-    let options_bar_logic = ui.global::<OptionsBarLogic>();
-    options_bar_logic.on_change_color_scheme({
-        let ui_weak = ui.as_weak();
-        move |cs| {
-            let ui = ui_weak.unwrap();
-            let app_theme = ui.global::<UAppTheme>();
-            app_theme.set_color_scheme(cs);
-        }
-    });
+    // let options_bar_logic = ui.global::<OptionsBarLogic>();
+    // options_bar_logic.on_change_color_scheme({
+    //     let ui_weak = ui.as_weak();
+    //     move |cs| {
+    //         let ui = ui_weak.unwrap();
+    //         let app_theme = ui.global::<UAppTheme>();
+    //         app_theme.set_color_scheme(cs);
+    //     }
+    // });
 
-    options_bar_logic.on_change_primary_color({
-        let ui_weak = ui.as_weak();
-        move |style| {
-            let ui = ui_weak.unwrap();
-            let app_theme = ui.global::<UAppTheme>();
-            app_theme.set_primary_color_style(style);
-        }
-    });
+    // options_bar_logic.on_change_primary_color({
+    //     let ui_weak = ui.as_weak();
+    //     move |style| {
+    //         let ui = ui_weak.unwrap();
+    //         let app_theme = ui.global::<UAppTheme>();
+    //         app_theme.set_primary_color_style(style);
+    //     }
+    // });
 
     ui.run()?;
 
