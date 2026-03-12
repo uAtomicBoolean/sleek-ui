8330fed1ce82fb15e7513e92c6e05976a7db402f
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2026-03-12 11:57:27 +0100

    Add accessability, settings and about pages

--- src/content/raw_content.rs
+++ src/content/raw_content.rs
@@ -1,12 +1,14 @@
 const HOW_TO_USE: &str = include_str!("../../docs-content/en/how-to-use.yaml");
 const THEMING: &str = include_str!("../../docs-content/en/theming.yaml");
 const DEFAULT_COLORS: &str = include_str!("../../docs-content/en/default-colors.yaml");
+const ACCESSABILITY: &str = include_str!("../../docs-content/en/accessability.yaml");
 
 pub fn get(page_id: &str) -> &str {
     match page_id {
         "how-to-use" => HOW_TO_USE,
         "theming" => THEMING,
         "default-colors" => DEFAULT_COLORS,
+        "accessability" => ACCESSABILITY,
         _ => "",
     }
 }
